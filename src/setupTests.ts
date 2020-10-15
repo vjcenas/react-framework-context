acb15a90ea5f85d43aa39f88de570b1f84e57901
Venstin John Cenas <vjcenas@gmail.com>
2020-10-15 20:09:30 +0800

    Update unit test

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -2,19 +2,3 @@ import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
-
-const mockFunction = () => {
-  const original = require.requireActual('react-router-dom');
-  return {
-    ...original,
-    useLocation: jest.fn().mockReturnValue({
-      pathname: '/',
-      search: '',
-      hash: '',
-      state: null,
-      key: '5nvxpbdafa',
-    }),
-  };
-};
-
-jest.mock('react-router-dom', () => mockFunction());
