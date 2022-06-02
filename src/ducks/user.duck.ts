193166379755bdba615aff48705d0bf6155661bf
Venstin John Cenas <vjcenas@yahoo.com>
2022-06-02 14:39:46 +0800

    Update user.duck.ts

--- src/ducks/user.duck.ts
+++ src/ducks/user.duck.ts
@@ -1,71 +1,103 @@
+import { ICommonState, IReducerAction } from 'src/libraries/thunk.library';
 import services from 'src/services/user.service';
 import { IUser } from 'src/models/user.model';
-import { ICommonState, IReducerAction } from 'src/libraries/thunk.library';
 
-export const actionTypes = {
-  USER_DATA_FETCH: 'USER_DATA_FETCH',
-  USER_LIST_FETCH: 'USER_LIST_FETCH',
+export const userActionTypes = {
+  USER_DATA_READ: 'USER_DATA_READ',
+  USER_LIST_READ: 'USER_LIST_READ',
+  USER_DATA_CREATE: 'USER_DATA_CREATE',
+  USER_DATA_UPDATE: 'USER_DATA_UPDATE',
+  USER_DATA_DELETE: 'USER_DATA_DELETE',
+  USER_DATA_SET: 'USER_DATA_SET',
   USER_ADD_AGE: 'USER_ADD_AGE',
 } as const;
 
-// This is where we put the actions that have promise/async payload like HTTP request
-export const asyncActions = {
+export const duckActions = {
   dataGET: {
-    type: actionTypes.USER_DATA_FETCH,
+    type: userActionTypes.USER_DATA_READ,
     service: services.dataGET,
   },
   listGET: {
-    type: actionTypes.USER_LIST_FETCH,
+    type: userActionTypes.USER_LIST_READ,
     service: services.listGET,
   },
-};
 
-// This is where we put the actions that doesn't have any promise/async payload
-export const syncActions = {
   addAge: (age: number) => ({
-    type: actionTypes.USER_ADD_AGE,
+    type: userActionTypes.USER_ADD_AGE,
     payload: age,
+  }),
+
+  // This is a sync action
+  setData: (user: IUser) => ({
+    type: userActionTypes.USER_DATA_SET,
+    payload: {
+      user,
+    },
   }),
 };
 
-export type IUserState = ICommonState<typeof actionTypes> & {
+export type IUserAsync = typeof duckActions;
+
+export interface IUserState extends ICommonState<typeof userActionTypes> {
   data?: IUser;
   list: IUser[];
-};
+  total: number;
+}
 
 export const defaultState: IUserState = {
   status: {},
   list: [],
+  total: 0,
 };
 
 const UserReducer = (
-  state = defaultState,
-  action: IReducerAction<typeof asyncActions & typeof syncActions>
+  state: IUserState,
+  action: IReducerAction<IUserAsync>
 ): IUserState => {
   switch (action.type) {
-    case actionTypes.USER_DATA_FETCH: {
+    case userActionTypes.USER_DATA_SET:
+    case userActionTypes.USER_DATA_READ:
+    case userActionTypes.USER_DATA_UPDATE:
+    case userActionTypes.USER_DATA_CREATE: {
       return {
         ...state,
-        data: action.payload,
+        data: action.payload?.user,
       };
     }
 
-    case actionTypes.USER_LIST_FETCH: {
+    case userActionTypes.USER_LIST_READ: {
       return {
         ...state,
-        list: action.payload ?? [],
+        list: action.payload?.rows ?? [],
+        total: action.payload?.count ?? 0,
       };
     }
 
-    case actionTypes.USER_ADD_AGE: {
-      return {
-        ...state,
-        data: {
-          ...(state.data as IUser),
-          age: Number(state.data?.age ?? 0) + Number(action.payload),
-        },
-      };
+    case userActionTypes.USER_DATA_DELETE: {
+      if (action.params) {
+        const [id] = action.params;
+        const list = state.list.filter((value) => value.userId !== id);
+
+        return {
+          ...state,
+          data: undefined,
+          total: state.total - (state.list.length - list.length),
+          list,
+        };
+      }
+
+      return state;
     }
+
+    case userActionTypes.USER_ADD_AGE: {
+        return {
+          ...state,
+          data: {
+            ...(state.data as IUser),
+            age: Number(state.data?.age ?? 0) + Number(action.payload),
+          },
+        };
+      }
 
     default: {
       return state;
